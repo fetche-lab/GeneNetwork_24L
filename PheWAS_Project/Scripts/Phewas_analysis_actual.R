bc2a7b0c265687e1170f0d40021ffa3b10871f73
fetche-lab <flisso434@gmail.com>
2025-03-13 08:02:47 +0300

    updates on phewas analysis on GN mice data

--- PheWAS_Project/Scripts/Phewas_analysis_actual.R
+++ PheWAS_Project/Scripts/Phewas_analysis_actual.R
@@ -1,8 +1,9 @@
 library(dplyr)
 
-input_dir <- "/home/fetche-lab/Desktop/phd_bioinformatics/PheWAS_Project/Data/Raw/phewas01/BTBD9_MICE"
-output_dir <- "/home/fetche-lab/Desktop/phd_bioinformatics/PheWAS_Project/Data/Processed"
-dir.create(output_dir, showWarnings=FALSE)
+input_dir <- "/home/fetche-lab/Desktop/gn_remote/GeneNetwork_24L/PheWAS_Project/Data/Raw/phewas01/MEIS1_MICE"
+output_dir <- "/home/fetche-lab/Desktop/gn_remote/GeneNetwork_24L/PheWAS_Project/Data/Processed/phewas01/MEIS1_MICE"
+output_dir1 <- "/home/fetche-lab/Desktop/gn_remote/GeneNetwork_24L/PheWAS_Project/Data/Processed/phewas01/MEIS1_MICE/filtered"
+#dir.create(output_dir, showWarnings=FALSE)
 
 csv_files <- list.files(input_dir, pattern="\\.csv$", full.names=TRUE)
 
@@ -12,8 +13,8 @@ process_file <- function(file_path) {
   data$Locus_at_Peak <- as.character(data$Locus_at_Peak)
   
   phewas_data <- data %>%
-    filter(Symbol == "Btbd9" & grepl("restless leg syndrome", Description, ignore.case=TRUE)) %>%
-    select(Record.ID, Locus_at_Peak, P_value_of_MAX)
+    filter(Symbol == "Meis1" & P_value_of_MAX < 0.05 & grepl("restless leg syndrome", Description, ignore.case=TRUE)) %>%
+    select(Record.ID, Locus_at_Peak, P_value_of_MAX, Description)
   
   output_file <- file.path(output_dir, paste0("phewas_", tools::file_path_sans_ext(basename(file_path)), ".csv"))
   write.csv(phewas_data, output_file, row.names=FALSE)
@@ -25,15 +26,15 @@ process_file <- function(file_path) {
 results <- lapply(csv_files, process_file)
 combined_data <- bind_rows(results)
 if (nrow(combined_data) > 0) {
-  write.csv(combined_data, file.path(output_dir, "phewas_btbd9_combined.csv"), row.names=FALSE)
+  write.csv(combined_data, file.path(output_dir1, "phewas_meis1_combined.csv"), row.names=FALSE)
   print(head(combined_data))
 }
 
 # Filter significant associations (p < 0.05)
-sig_hits <- combined_data %>%
-  filter(P_value_of_MAX < 0.05)
+#sig_hits <- combined_data %>%
+  #filter(P_value_of_MAX < 0.05)
 
-write.csv(sig_hits, "phewas_btbd9_combined-filtered.csv", row.names=FALSE)
+#write.csv(sig_hits, "phewas_btbd9_combined-filtered.csv", row.names=FALSE)
 
 
 # PheWAS plot for combined data (Optional)
